30fa68ba12896f9eb38471d40e89a0c0a798e999
zhanyar004 <119080464+zhanyar004@users.noreply.github.com>
2022-12-23 16:35:18 +0300

    Update apps.js

--- apps/apps.js
+++ apps/apps.js
@@ -1,6 +1,6 @@
-function vshack(){window.open("","_self")}
-function vspro(){window.open("itms-services://?action=download-manifest&amp;url=https://ittza7aa.com/plist/insta","_self")}
-function snapx(){window.open("itms-services://?action=download-manifest&amp;url=https://ittza7aa.com/plist/tiktok","_self")}
-function insta(){window.open("itms-services://?action=download-manifest&amp;url=https://ittza7aa.com/plist/vshack.plist","_self")}
-function tiktoklrd(){window.open("itms-services://?action=download-manifest&amp;url=https://ittza7aa.com/plist/vspro","_self")}
-function youtube(){window.open("itms-services://?action=download-manifest&amp;url=https://ittza7aa.com/plist/djay","_self")}
+function vshack(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/vshack","_self")}
+function vspro(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/vspro","_self")}
+function snapx(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/snapx","_self")}
+function insta(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/insta.plist","_self")}
+function tiktoklrd(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/tiktoklrd","_self")}
+function youtube(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/youtube","_self")}
