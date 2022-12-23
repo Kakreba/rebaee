cebf0dba76598f2973e797c092b3a34de7f351d8
zhanyar004 <119080464+zhanyar004@users.noreply.github.com>
2022-12-24 00:45:10 +0300

    Update apps.js

--- ipa/apps.js
+++ ipa/apps.js
@@ -12,11 +12,11 @@ function snapx(){window.open("https://archive.org/download/snapchatx-zhanyar/Sna
 
 
 
-function instagramplus(){window.open("","_self")}
+function instagramplus(){window.open("https://archive.org/download/20221223_20221223_1356/DLInsta_264.0_com.burbn.instagram_iPhone%20Distribution%3A%20Beijing%20Whaty%20Technology%20Development%20Co.%2CLtd._1671801290.ipa","_self")}
 function tikplus(){window.open("","_self")}
-function tikLRD(){window.open("","_self")}
+function tikLRD(){window.open("https://archive.org/download/tiktoklrd_202212/TikTok%20LRD_27.2.0_com.zhiliaoapp.musically_iPhone%20Distribution%3A%20Beijing%20Whaty%20Technology%20Development%20Co.%2CLtd._1671801689.ipa","_self")}
 function TikAC(){window.open("","_self")}
-function youtubeplus(){window.open("","_self")}
+function youtubeplus(){window.open("https://archive.org/download/dltube-17.49.6-com.google.ios.youtube-i-phone-distribution-beijing-whaty-technol/DLTube_17.49.6_com.google.ios.youtube_iPhone%20Distribution%3A%20Beijing%20Whaty%20Technology%20Development%20Co.%2CLtd._1671801389.ipa","_self")}
 function Aloha(){window.open("","_self")}
 function FakeCall(){window.open("","_self")}
 function RadarBot(){window.open("","_self")}
