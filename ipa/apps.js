eb173bb9725becc2d8144c8b998907ba20f11bc3
zhanyar004 <119080464+zhanyar004@users.noreply.github.com>
2022-12-24 00:38:42 +0300

    Update apps.js

--- ipa/apps.js
+++ ipa/apps.js
@@ -1,9 +1,37 @@
-function uncover(){window.open("","_self")}
-function insta(){window.open("itms-services://?action=download-manifest&amp;url=https://ittza7aa.com/plist/insta","_self")}
-function tiktok(){window.open("itms-services://?action=download-manifest&amp;url=https://ittza7aa.com/plist/tiktok","_self")}
-function vshack(){window.open("itms-services://?action=download-manifest&amp;url=https://ittza7aa.com/plist/vshack.plist","_self")}
-function vspro(){window.open("itms-services://?action=download-manifest&amp;url=https://ittza7aa.com/plist/vspro","_self")}
-function djay(){window.open("itms-services://?action=download-manifest&amp;url=https://ittza7aa.com/plist/djay","_self")}
-function cute(){window.open("itms-services://?action=download-manifest&amp;url=https://ittza7aa.com/plist/cute","_self")}
-function picsart(){window.open("itms-services://?action=download-manifest&amp;url=https://ittza7aa.com/plist/picsart","_self")}
-function vspro12(){window.open("itms-services://?action=download-manifest&amp;url=https://ittza7aa.com/plist/vspro12","_self")}
+function Fx(){window.open("","_self")}
+function djpro(){window.open("","_self")}
+function Ccpro(){window.open("","_self")}
+function ms(){window.open("","_self")}
+function vn(){window.open("","_self")}
+function lumaf(){window.open("","_self")}
+function funimate(){window.open("","_self")}
+function snapplus(){window.open("","_self")}
+function snapx(){window.open("","_self")}
+
+
+
+
+
+function instagramplus(){window.open("","_self")}
+function tikplus(){window.open("","_self")}
+function tikLRD(){window.open("","_self")}
+function TikAC(){window.open("","_self")}
+function youtubeplus(){window.open("","_self")}
+function Aloha(){window.open("","_self")}
+function FakeCall(){window.open("","_self")}
+function RadarBot(){window.open("","_self")}
+function AdGuard(){window.open("","_self")}
+
+
+
+
+
+function photomath(){window.open("","_self")}
+function picsart(){window.open("","_self")}
+function quran(){window.open("","_self")}
+function (){window.open("","_self")}
+function (){window.open("","_self")}
+function (){window.open("","_self")}
+function (){window.open("","_self")}
+function (){window.open("","_self")}
+function (){window.open("","_self")}
