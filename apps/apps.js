1731fa3bef0a529429fa32118857c778da34d1e0
zhanyar004 <119080464+zhanyar004@users.noreply.github.com>
2022-12-31 22:40:03 +0300

    Update apps.js

--- apps/apps.js
+++ apps/apps.js
@@ -1,6 +1,73 @@
 function vshack(){window.open("itms-services://?action=download-manifest&url=https://plist.tweakdoor.com/videostarold","_self")}
+
+
 function vspro(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/vspro.plist","_self")}
-function snapx(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/snapx.plist","_self")}
-function insta(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/insta.plist","_self")}
-function tiktoklrd(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/tiktoklrd.plist","_self")}
+
+
+function videofx(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/snapx.plist","_self")}
+
+
+function djay(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/insta.plist","_self")}
+
+
+function cutecut(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/tiktoklrd.plist","_self")}
+
+
+function moviespirit(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/youtube.plist","_self")}
+
+
+function vnvideo(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/tiktoklrd.plist","_self")}
+
+
+function luma(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/youtube.plist","_self")}
+
+
+function funimate(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/youtube.plist","_self")}
+
+
+function snap++(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/youtube.plist","_self")}
+
+
+function snapx(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/youtube.plist","_self")}
+
+
+function insta(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/youtube.plist","_self")}
+
+
+function tiktokplus(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/youtube.plist","_self")}
+
+
+function tiktoklrd(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/youtube.plist","_self")}
+
+
+function tiktokAUTO(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/youtube.plist","_self")}
+
+
 function youtube(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/youtube.plist","_self")}
+
+
+function aloha(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/youtube.plist","_self")}
+
+
+function fakecall(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/youtube.plist","_self")}
+
+
+function radarbot(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/youtube.plist","_self")}
+
+
+function ADguard(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/youtube.plist","_self")}
+
+
+function Photomath(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/youtube.plist","_self")}
+
+
+function picsart(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/youtube.plist","_self")}
+
+
+function qwran(){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/youtube.plist","_self")}
+
+
+function (){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/youtube.plist","_self")}
+
+
+function (){window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/plist/youtube.plist","_self")}
