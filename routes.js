40ebf4dcc8f1da0f50bd63d2242e07b7f7d5eebb
zhanyarios004gardi004 <119080464+zhanyarios004gardi004@users.noreply.github.com>
2023-01-20 15:26:52 +0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,203 +1,40 @@
-"use strict";
-var routes = [
-  {
-  path: '/',
-  Url: './index.html',
-  },
-  {
-    path: './plist/',
-    url: './404.html',
-  },
-  {
-    path: '/plist/vshack',
-    url: './plist/vshack.html',
-  },
-  {
-    path: '/buttons/',
-    url: './elements/buttons.html',
-  },
-  {
-    path: '/cards/',
-    url: './elements/cards.html',
-  },
-  {
-    path: '/icons/',
-    url: './elements/icons.html',
-  },
-  {
-    path: '/list-view/',
-    url: './elements/list-view.html',
-  },
-  {
-    path: '/popover/',
-    url: './elements/popover.html',
-  },
-  {
-    path: '/popup/',
-    url: './elements/popup.html',
-  },
-  {
-    path: '/accordion/',
-    url: './elements/accordion.html',
-  },
-  {
-    path: '/slider/',
-    url: './elements/slider.html',
-  },
-  {
-    path: '/tooltip/',
-    url: './elements/tooltip.html',
-  },
-  {
-    path: '/about-us/',
-    url: './pages/about-us.html',
-  },
-  {
-    path: '/blog/',
-    url: './pages/blog.html',
-  },
+var app = new Framework7({
+  routes: [
     {
-    path: '/web/',
-    url: './pages/web.html',
-  },
-  {
-    path: '/web1/',
-    url: './pages/web1.html',
-  },
-   {
-    path: '/python/',
-    url: './pages/python.html',
-  },
-  {
-    path: '/blog-single/',
-    url: './pages/blog-single.html',
-  },
-  {
-    path: '/career/',
-    url: './pages/career.html',
-  },
-  {
-    path: '/client/',
-    url: './pages/client.html',
-  },
+      name: 'vshack',
+      path: '/plist/vshack',
+      url: './plist/vshack.html',
+    },
     {
-    path: '/ttt/',
-    url: './pages/tttt.html',
-  },
+      name: 'news',
+      path: '/news/',
+      url: './pages/news.html',
+      options: {
+        animate: false,
+      },
+    },
     {
-    path: '/jb/',
-    url: './pages/jb.html',
-  },
-	{
-    path: '/apple/',
-    url: './pages/apple.html',
-  },
-      {
-    path: '/andr/',
-    url: './pages/andr.html',
-  },
-        {
-    path: '/tools/',
-    url: './pages/tools.html',
-  },
-   {
-    path: '/what/',
-    url: './pages/what.html',
-  },
-  {
-    path: '/visa/',
-    url: './pages/visa.html',
-  },
-  {
-    path: '/features/',
-    url: './pages/features.html',
-  },
-  {
-    path: '/gallery/',
-    url: './pages/gallery.html',
-  },
-  {
-    path: '/maintenance/',
-    url: './pages/maintenance.html',
-  },
-  {
-    path: '/page-not-found/',
-    url: './pages/page-not-found.html',
-  },
-  {
-    path: '/pricing-table/',
-    url: './pages/pricing-table.html',
-  },
-  {
-    path: '/services/',
-    url: './pages/services.html',
-  },
-  {
-    path: '/appios/',
-    url: './pages/skill.html',
-  },
-	{
-    path: '/ios/',
-    url: './pages/skill1.html',
-			},	
-  {
-    path: '/walkthrough/',
-    url: './apps/walkthrough.html',
-  },
-  {
-    path: '/login/',
-    url: './apps/login.html',
-  },
-  {
-    path: '/register/',
-    url: './apps/register.html',
-  },
-  {
-    path: '/reset-password/',
-    url: './apps/reset-password.html',
-  },
-  {
-    path: '/contact-us/',
-    url: './apps/contact-us.html',
-  },
-  {
-    path: '/calendar/',
-    url: './apps/calendar.html',
-  },
-  {
-    path: '/profile/',
-    url: './apps/profile.html',
-  },
-  {
-    path: '/profile-edit/',
-    url: './apps/profile-edit.html',
-  },
-  {
-    path: '/embeds/',
-    url: './apps/embeds.html',
-  },
-  {
-    path: '/faq/',
-    url: './apps/faq.html',
-  },
-  {
-    path: '/maps/',
-    url: './apps/maps.html',
-  },
-  {
-    path: '/logout/',
-    url: './apps/logout.html',
-  },
+      name: 'users',
+      path: '/users/',
+      componentUrl: './pages/users.html',
+      options: {
+        props: {
+          users: ['John Doe', 'Vladimir Kharlampidi', 'Timo Ernst'],
+        },
+      },
+      on: {
+        pageAfterIn: function test (e, page) {
+          // do something after page gets into the view
+        },
+        pageInit: function (e, page) {
+          // do something when page initialized
+        },
+      }
+    },
+    // Default route, match to all pages (e.g. 404 page)
     {
-    path: '/html3/',
-    url: './index3.html/',
-  },
-      {
-    path: '/ud/',
-    url: './apps/ud.html/',
-  },
-  {
-    path: '/unc/',
-    url: './plists/unc.plist/',
-  },
-];
+      path: '(.*)',
+      url: './pages/404.html',
+    },
+  ],
+});
