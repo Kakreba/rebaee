09c3b5533fa9e9975da2964ac789e1d2ed149a25
zhanyar004 <zhanyar.store@gmail.com>
2023-01-20 03:51:01 +0000

    b

--- js/routes.js
+++ js/routes.js
@@ -1,25 +1,203 @@
-const route = (event) => {
-    event = event || window.event;
-    event.preventDefault();
-    window.history.pushState({}, "", event.target.href);
-    handleLocation();
-};
-
-const routes = {
-    404: "/404.html",
-    "/": "/pages/index.html",
-    "/plist/vshack": "/plist/vshack.html",
-    "/plist": "/404.html",
-};
-
-const handleLocation = async () => {
-    const path = window.location.pathname;
-    const route = routes[path] || routes[404];
-    const html = await fetch(route).then((data) => data.text());
-    document.getElementById("main-page").innerHTML = html;
-};
-
-window.onpopstate = handleLocation;
-window.route = route;
-
-handleLocation();
+"use strict";
+var routes = [
+  {
+  path: '/',
+  Url: './index.html',
+  },
+  {
+    path: '/plist/',
+    url: '/404.html',
+  },
+  {
+    path: '/plist/vshack',
+    url: '/plist/vshack.html',
+  },
+  {
+    path: '/buttons/',
+    url: '/elements/buttons.html',
+  },
+  {
+    path: '/cards/',
+    url: './elements/cards.html',
+  },
+  {
+    path: '/icons/',
+    url: './elements/icons.html',
+  },
+  {
+    path: '/list-view/',
+    url: './elements/list-view.html',
+  },
+  {
+    path: '/popover/',
+    url: './elements/popover.html',
+  },
+  {
+    path: '/popup/',
+    url: './elements/popup.html',
+  },
+  {
+    path: '/accordion/',
+    url: './elements/accordion.html',
+  },
+  {
+    path: '/slider/',
+    url: './elements/slider.html',
+  },
+  {
+    path: '/tooltip/',
+    url: './elements/tooltip.html',
+  },
+  {
+    path: '/about-us/',
+    url: './pages/about-us.html',
+  },
+  {
+    path: '/blog/',
+    url: './pages/blog.html',
+  },
+    {
+    path: '/web/',
+    url: './pages/web.html',
+  },
+  {
+    path: '/web1/',
+    url: './pages/web1.html',
+  },
+   {
+    path: '/python/',
+    url: './pages/python.html',
+  },
+  {
+    path: '/blog-single/',
+    url: './pages/blog-single.html',
+  },
+  {
+    path: '/career/',
+    url: './pages/career.html',
+  },
+  {
+    path: '/client/',
+    url: './pages/client.html',
+  },
+    {
+    path: '/ttt/',
+    url: './pages/tttt.html',
+  },
+    {
+    path: '/jb/',
+    url: './pages/jb.html',
+  },
+	{
+    path: '/apple/',
+    url: './pages/apple.html',
+  },
+      {
+    path: '/andr/',
+    url: './pages/andr.html',
+  },
+        {
+    path: '/tools/',
+    url: './pages/tools.html',
+  },
+   {
+    path: '/what/',
+    url: './pages/what.html',
+  },
+  {
+    path: '/visa/',
+    url: './pages/visa.html',
+  },
+  {
+    path: '/features/',
+    url: './pages/features.html',
+  },
+  {
+    path: '/gallery/',
+    url: './pages/gallery.html',
+  },
+  {
+    path: '/maintenance/',
+    url: './pages/maintenance.html',
+  },
+  {
+    path: '/page-not-found/',
+    url: './pages/page-not-found.html',
+  },
+  {
+    path: '/pricing-table/',
+    url: './pages/pricing-table.html',
+  },
+  {
+    path: '/services/',
+    url: './pages/services.html',
+  },
+  {
+    path: '/appios/',
+    url: './pages/skill.html',
+  },
+	{
+    path: '/ios/',
+    url: './pages/skill1.html',
+			},	
+  {
+    path: '/walkthrough/',
+    url: './apps/walkthrough.html',
+  },
+  {
+    path: '/login/',
+    url: './apps/login.html',
+  },
+  {
+    path: '/register/',
+    url: './apps/register.html',
+  },
+  {
+    path: '/reset-password/',
+    url: './apps/reset-password.html',
+  },
+  {
+    path: '/contact-us/',
+    url: './apps/contact-us.html',
+  },
+  {
+    path: '/calendar/',
+    url: './apps/calendar.html',
+  },
+  {
+    path: '/profile/',
+    url: './apps/profile.html',
+  },
+  {
+    path: '/profile-edit/',
+    url: './apps/profile-edit.html',
+  },
+  {
+    path: '/embeds/',
+    url: './apps/embeds.html',
+  },
+  {
+    path: '/faq/',
+    url: './apps/faq.html',
+  },
+  {
+    path: '/maps/',
+    url: './apps/maps.html',
+  },
+  {
+    path: '/logout/',
+    url: './apps/logout.html',
+  },
+    {
+    path: '/html3/',
+    url: './index3.html/',
+  },
+      {
+    path: '/ud/',
+    url: './apps/ud.html/',
+  },
+  {
+    path: '/unc/',
+    url: './plists/unc.plist/',
+  },
+];
