88018dc31e0d22102d92c5ed1f91b8b59e086c20
zhanyarios004gardi004 <119080464+zhanyarios004gardi004@users.noreply.github.com>
2023-01-20 15:27:11 +0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -34,7 +34,7 @@ var app = new Framework7({
     // Default route, match to all pages (e.g. 404 page)
     {
       path: '(.*)',
-      url: './pages/404.html',
+      url: './404.html',
     },
   ],
 });
