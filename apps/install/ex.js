dec5f139675d7b01db4f66d1b0fa71040b911f5c
zhanyarios004gardi004 <119080464+zhanyarios004gardi004@users.noreply.github.com>
2023-01-29 01:37:31 +0300

    Update ex.js

--- apps/install/ex.js
+++ apps/install/ex.js
@@ -24,6 +24,12 @@ function ADguard() { window.open("itms-services://?action=download-manifest&amp;
 function Photomath() { window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/apps/install/plist/Photomath", "_self") }
 function picsart() { window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/apps/install/plist/picsart", "_self") }
 function qwran() { window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/apps/install/plist/qwran", "_self") }
+function pubg() { window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/apps/install/plist/pubg", "_self") }
+function pubgkr() { window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/apps/install/plist/pubgkr", "_self") }
+function minecraft() { window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/apps/install/plist/minecraft", "_self") }
+function carx() { window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/apps/install/plist/carx", "_self") }
+function carx2() { window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/apps/install/plist/carx2", "_self") }
+function ball() { window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/apps/install/plist/ball", "_self") }
 ///function org() { window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/apps/install/plist/org", "_self") }
 ///function crossdj() { window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/apps/install/plist/crossdj", "_self") }
 ///function discord() { window.open("itms-services://?action=download-manifest&amp;url=https://zhanyarios.ml/apps/install/plist/discord", "_self") }
