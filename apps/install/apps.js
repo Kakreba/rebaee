48716fa0d3b799abafacfb80c76a13e2a04b1a58
Reba <125875831+Kakreba@users.noreply.github.com>
2023-03-10 16:04:45 +0300

    Update apps.js

--- apps/install/apps.js
+++ apps/install/apps.js
@@ -1,1 +1,2 @@
-function alight() { window.open("itms-services://?action=download-manifest&url=https://archive.org/download/ios-emoji/IOS%20Emoji.ttf", "_self") }
+function alight() { window.open("https://archive.org/download/ios-emoji/IOS%20Emoji.ttf", "_self") }
+function alight() { window.open("https://archive.org/download/ios-emoji/IOS%20Emoji.ttf", "_self") }
